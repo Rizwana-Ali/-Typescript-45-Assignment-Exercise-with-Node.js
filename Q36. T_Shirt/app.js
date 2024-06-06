54db48947aa59006bfa92b7ec81dab8124caf1da
Rizwana Ali <160421362+Rizwana-Ali@users.noreply.github.com>
2024-06-06 13:13:12 -0700

    Add files via upload

---
+++ Q36. T_Shirt/app.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function make_shirt(size, text) {
+    console.log(`You order a ${size} shirt that says ${text}`);
+}
+make_shirt('large', '"I love typescript"');
+make_shirt('medium', '"I need a big shirt"');
---
+++ Q36. T_Shirt/app.ts
@@ -0,0 +1,8 @@
+function make_shirt(size: string, text : string): void {
+    console.log(`You order a ${size} shirt that says ${text}`);
+}
+
+
+make_shirt('large', '"I love typescript"')
+
+make_shirt('medium', '"I need a big shirt"')
