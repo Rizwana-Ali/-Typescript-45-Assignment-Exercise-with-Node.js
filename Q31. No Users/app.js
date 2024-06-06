97b8ceac4128caaf18d73a0ff558fc7c170cc998
Rizwana Ali <160421362+Rizwana-Ali@users.noreply.github.com>
2024-06-06 13:40:30 -0700

    Add files via upload

---
+++ Q31. No Users/app.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let users = ["Armish", "Arsalan", "Imtiaz", "Bilawal"];
+while (users.length > 0) {
+    users.pop();
+}
+if (users.length === 0) {
+    console.log("we need to find some users!");
+}
---
+++ Q31. No Users/app.ts
@@ -0,0 +1,11 @@
+let userNames:string [] = ["admin", "shan","danial", "shayan", "humza"];
+
+
+while (userNames.length > 0){
+    userNames.pop()
+}
+
+if(userNames.length === 0){
+    console.log("we need to find some users!");
+}
+
