779c082eb3d266fde581f6c32c84a43154233dba
Rizwana Ali <160421362+Rizwana-Ali@users.noreply.github.com>
2024-06-06 13:29:21 -0700

    Add files via upload

---
+++ Q33.Ordinal Numbers/app.js
@@ -0,0 +1,29 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let ordianal_Array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+for (let i = 0; i < ordianal_Array.length; i++) {
+    if (ordianal_Array[i] === 1) {
+        console.log(`${ordianal_Array[i]}st`);
+    }
+    else if (ordianal_Array[i] === 2) {
+        console.log(`${ordianal_Array[i]}nd`);
+    }
+    else if (ordianal_Array[i] === 3) {
+        console.log(`${ordianal_Array[i]}rd`);
+    }
+    else if (ordianal_Array[i] === 4) {
+        console.log(`${ordianal_Array[i]}th`);
+    }
+    else if (ordianal_Array[i] === 5) {
+        console.log(`${ordianal_Array[i]}th`);
+    }
+    else if (ordianal_Array[i] === 6) {
+        console.log(`${ordianal_Array[i]}th`);
+    }
+    else if (ordianal_Array[i] === 8) {
+        console.log(`${ordianal_Array[i]}th`);
+    }
+    else if (ordianal_Array[i] === 9) {
+        console.log(`${ordianal_Array[i]}th`);
+    }
+}
