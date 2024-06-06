0056c7e04afbf4bc149ba244f14208cd24ce5444
Rizwana Ali <160421362+Rizwana-Ali@users.noreply.github.com>
2024-06-06 13:23:45 -0700

    Add files via upload

---
+++ Q34. Pizzas/app.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let favourite_pizza = ["pepperoni", "chicken", "veg"];
+for (let pizza of favourite_pizza) {
+    console.log(pizza);
+}
+console.log("\n");
+for (let pizza of favourite_pizza) {
+    console.log(`I really like ${pizza} pizza!`);
+}
+console.log("\nI really love pizza!");
