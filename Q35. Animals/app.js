7223dd747ca2a36a86c4124726f0ed130c443524
Rizwana Ali <160421362+Rizwana-Ali@users.noreply.github.com>
2024-06-06 13:19:01 -0700

    Add files via upload

---
+++ Q35. Animals/app.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let animals = ["cat", "tiger", "Dog"];
+for (let animal of animals) {
+    console.log(animal);
+}
+console.log("\n");
+for (let animal of animals) {
+    // console.log(`A ${animal} has a tail`)
+    console.log(`A ${animal} would make a agreat pet.`);
+}
+console.log("\n Any of these animals would make a great pets! but i love cats more");
---
+++ Q35. Animals/app.ts
@@ -0,0 +1,14 @@
+let animals : string [] = ["cat","tiger","Dog"]
+
+for (let animal of animals){
+    console.log(animal);
+}
+
+console.log("\n");
+
+for(let animal of animals){
+
+    console.log(`A ${animal} would make a agreat pet.`);
+}
+
+console.log("\n Any of these animals would make a great pet!");
