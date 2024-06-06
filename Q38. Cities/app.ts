c150cb9136253a08d8fd1900fc26e764ef221561
Rizwana Ali <160421362+Rizwana-Ali@users.noreply.github.com>
2024-06-06 12:26:50 -0700

    Add files via upload

---
+++ Q38. Cities/app.ts
@@ -0,0 +1,9 @@
+function describe_city(city: string, country: string = 'Pakistan'): void {
+    console.log(`${city} is in ${country}`)
+    
+    }
+    
+    describe_city("Karachi");
+    describe_city("Berlin", "Germany");
+    describe_city("newyork", "America");
+    
