961910aa4686ec360ff55fcac609a6c851b52f3f
Rizwana Ali <160421362+Rizwana-Ali@users.noreply.github.com>
2024-06-07 00:36:20 -0700

    Add files via upload

---
+++ Q21.Store Typescript in Object/app.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// person object
+let person = { name: "subhan", age: 21, student: true };
+console.log(person);
+// car object
+let car = {
+    maker: 'toyota',
+    color: 'black',
+    automatic: true
+};
+// print car
+console.log(car);
---
+++ Q21.Store Typescript in Object/app.ts
@@ -0,0 +1,23 @@
+// person object
+let person:{name:string, age:number, student:boolean}  =  {name: "subhan", age: 21, student: true};
+
+console.log(person);
+
+// Data type of car object
+interface car{
+     maker : string,
+     color: string,
+     automatic :boolean
+}
+
+// car object
+let car = {
+    maker : 'toyota',
+     color: 'black',
+     automatic :true
+}
+
+// print car
+console.log(car);
+
+
