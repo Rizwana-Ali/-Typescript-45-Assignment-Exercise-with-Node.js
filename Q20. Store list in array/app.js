f6f183c09fbe69c6b43c8657d94c52422e8408e6
Rizwana Ali <160421362+Rizwana-Ali@users.noreply.github.com>
2024-06-07 00:47:51 -0700

    Add files via upload

---
+++ Q20. Store list in array/app.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let languages = ["french", "spanish", "sindhi", "panjabi", "pashto", "urdu",];
+console.log("List of languages");
+languages.map((item) => console.log(item));
---
+++ Q20. Store list in array/app.ts
@@ -0,0 +1,5 @@
+let languages:string[] = ["french" , "spanish" , "sindhi", "panjabi" , "pashto" , "urdu",];
+
+console.log("List of languages");
+
+languages.map((item)=> console.log(item));
