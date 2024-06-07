a9af09e7d1b97dd402ab43aa9d3a02f230d21279
Rizwana Ali <160421362+Rizwana-Ali@users.noreply.github.com>
2024-06-06 23:51:16 -0700

    Add files via upload

---
+++ Q27. if else With Colour#3/app.js
@@ -0,0 +1,35 @@
+"use strict";
+// Virsion 1 of the program
+Object.defineProperty(exports, "__esModule", { value: true });
+let alien_color = "green";
+if (alien_color === "green") {
+    console.log("( virsion 1) Alien color is green, player just earned  5 points.");
+}
+else if (alien_color === "yellow") {
+    console.log(" (virsion 1) Alien color is yellow, player just earned  10 points.");
+}
+else if (alien_color === "red") {
+    console.log("(virsion 1) Alien color is red, player just earned  15 points.");
+}
+//  Virsion 2 of the program
+let alien_Color = "yellow";
+if (alien_Color === "green") {
+    console.log("( virsion 2) Alien color is green, player just earned  5 points.");
+}
+else if (alien_Color === "yellow") {
+    console.log(" (virsion 2) Alien color is yellow, player just earned  10 points.");
+}
+else if (alien_Color === "red") {
+    console.log("(virsion 2) Alien color is red, player just earned  15 points.");
+}
+//  Virsion 3 of the program
+let aliencolor = "red";
+if (aliencolor === "green") {
+    console.log("( virsion 3) Alien color is green, player just earned  5 points.");
+}
+else if (aliencolor === "yellow") {
+    console.log(" (virsion 3) Alien color is yellow, player just earned  10 points.");
+}
+else if (aliencolor === "red") {
+    console.log("(virsion 3) Alien color is red, player just earned  15 points.");
+}
