f764924ac547dc888a1af198fb4e6150e92b2851
Rizwana Ali <160421362+Rizwana-Ali@users.noreply.github.com>
2024-06-06 23:32:33 -0700

    Add files via upload

---
+++ Q29. Favorite  Fruit/app.js
@@ -0,0 +1,18 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const favouriteFruites = ["Banana", "Apple", "Strawberry"];
+if (favouriteFruites.includes("Banana")) {
+    console.log("I really like Bananas.");
+}
+if (favouriteFruites.includes("Apple")) {
+    console.log("I really like Apple.");
+}
+if (favouriteFruites.includes("orange")) {
+    console.log("I really like orange.");
+}
+if (favouriteFruites.includes("Strawberry")) {
+    console.log("I really like Strawberries");
+}
+if (favouriteFruites.includes("Grapes")) {
+    console.log("I really like Grapes.");
+}
---
+++ Q29. Favorite  Fruit/app.ts
@@ -0,0 +1,22 @@
+const favouriteFruites: string [] = ["Banana", "Apple", "Strawberry"];
+
+if(favouriteFruites.includes("Banana")){
+    console.log("I really like Bananas.");
+}
+
+if(favouriteFruites.includes("Apple")){
+    console.log("I really like Apple.");
+}
+if(favouriteFruites.includes("orange")){
+    console.log("I really like orange.");
+}
+
+if(favouriteFruites.includes("Strawberry")){
+    console.log("I really like Strawberries");
+}
+
+if(favouriteFruites.includes("Grapes")){
+    console.log("I really like Grapes.");
+}
+
+
