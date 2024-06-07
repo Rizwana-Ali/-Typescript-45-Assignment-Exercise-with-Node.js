8b01be3b44c53ece4dd0adde4f36982a8d5b2615
Rizwana Ali <160421362+Rizwana-Ali@users.noreply.github.com>
2024-06-06 23:57:04 -0700

    Add files via upload

---
+++ Q26. if else With Colour#2/app.js
@@ -0,0 +1,18 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// first virsion
+let alien_Color = "green";
+if (alien_Color === "green") {
+    console.log("Alien color is green, player just earned 5 points for shooting the alien.");
+}
+else {
+    console.log(" Alien color is not green, player just earned 10  points.");
+}
+// second version
+let alienColor = "red";
+if (alienColor === "green") {
+    console.log("Alien color is green, player just earned 5 points for shooting the alien.");
+}
+else {
+    console.log(" Alien color is not green, player just earned 10  points.");
+}
---
+++ Q26. if else With Colour#2/app.ts
@@ -0,0 +1,18 @@
+// first virsion
+let alien_Color:string = "green";
+if (alien_Color ==="green"){
+    console.log("Alien color is green, player just earned 5 points for shooting the alien.");
+}
+else{
+console.log(" Alien color is not green, player just earned 10  points.");
+}
+
+
+// second version
+let alienColor:string = "red";
+if (alienColor ==="green"){
+    console.log("Alien color is green, player just earned 5 points for shooting the alien.");
+}
+else{
+console.log(" Alien color is not green, player just earned 10  points.");
+}
