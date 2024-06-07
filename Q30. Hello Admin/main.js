cde2d8ff7b5ebcddb8d98c5c93fe93d14ebaa0e2
Rizwana Ali <160421362+Rizwana-Ali@users.noreply.github.com>
2024-06-06 23:25:45 -0700

    Add files via upload

---
+++ Q30. Hello Admin/main.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let userNames = ["admin", "shan", "danial", "shayan", "humza"];
+for (let i = 0; i < userNames.length; i++) {
+    if (userNames[i] == "admin") {
+        console.log("Hello admin, would you like to see  a status report?");
+    }
+    else {
+        console.log(`hello ${userNames[i]},thank you for logging in again.`);
+    }
+}
