74ca9d1f72e5609c5795cf5b2e13f4fecc98101a
Rizwana Ali <160421362+Rizwana-Ali@users.noreply.github.com>
2024-06-07 01:40:21 -0700

    Add files via upload

---
+++ Q13. Your_Own_Array/app.js
@@ -0,0 +1,4 @@
+let transportation = ["Honda Motercycle", "Honda City", "Honda Civic", "BMW"];
+transportation.map((item) => console.log("I WOULD LIKE TO OWN A ", item));
+transportation.map((item) => console.log(`I WOULD LIKE TO OWN A ,${item}`));
+export {};
---
+++ Q13. Your_Own_Array/app.ts
@@ -0,0 +1,6 @@
+let transportation:string[] = ["Honda Motercycle","Honda City","Honda Civic","BMW"];
+
+transportation.map((item)=> console.log("I WOULD LIKE TO OWN A ",item));
+
+
+
