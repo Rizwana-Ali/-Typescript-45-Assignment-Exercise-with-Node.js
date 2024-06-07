646991b11c87aa7a429ae35bca6c147f31fa63c7
Rizwana Ali <160421362+Rizwana-Ali@users.noreply.github.com>
2024-06-07 03:07:36 -0700

    Add files via upload

---
+++ Q7. Number Eight/app.js
@@ -0,0 +1,19 @@
+// Addition
+// let a:number = 7;
+// let b:number = 1;
+// console.log(a+b)
+let addition = 7 + 1;
+console.log(`addition ${addition}`);
+//  Subtraction
+// let c:number = 9;
+// let d:number = 1;
+// console.log(c-d);
+let subtraction = 12 - 4;
+console.log("Subtraction", subtraction);
+// Multiplication
+let multiplication = 4 * 2;
+console.log("multiplication", multiplication);
+// Division
+let division = 16 / 2;
+console.log("Division", division);
+export {};
---
+++ Q7. Number Eight/app.ts
@@ -0,0 +1,31 @@
+// Addition
+
+// let a:number = 7;
+// let b:number = 1;
+// console.log(a+b)
+ 
+let addition:number = 7+1;
+console.log(`addition ${addition}`);
+
+
+//  Subtraction
+
+// let c:number = 9;
+// let d:number = 1;
+// console.log(c-d);
+
+let subtraction:number = 12-4;
+
+console.log("Subtraction", subtraction);
+
+
+// Multiplication
+
+let multiplication:number = 4*2;
+console.log("multiplication",multiplication);
+
+// Division
+let division:number = 16/2;
+
+console.log("Division",division);
+
