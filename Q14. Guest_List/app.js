aff40f150fc8ad6b965d7f38e10cfa0397b9a58b
Rizwana Ali <160421362+Rizwana-Ali@users.noreply.github.com>
2024-06-07 01:36:07 -0700

    Add files via upload

---
+++ Q14. Guest_List/app.js
@@ -0,0 +1,4 @@
+let guestarray = ["haya", "aresha", "rameema"];
+// guestarray.map((item)=> console.log(`Hello,${item} you are invited to dinner.`));
+guestarray.forEach(oneGuest => console.log(`Hello,${oneGuest} you are invited to dinner.`));
+export {};
---
+++ Q14. Guest_List/app.ts
@@ -0,0 +1,5 @@
+let guestarray:string[] = [ "haya", "aresha", "rameema" ];
+
+// guestarray.map((item)=> console.log(`Hello,${item} you are invited to dinner.`));
+
+guestarray.forEach(oneGuest => console.log(`Hello,${oneGuest} you are invited to dinner.`))
