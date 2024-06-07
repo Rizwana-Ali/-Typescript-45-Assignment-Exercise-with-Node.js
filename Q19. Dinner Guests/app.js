4301032b2c554ce40bec67709300e6c94019b2e6
Rizwana Ali <160421362+Rizwana-Ali@users.noreply.github.com>
2024-06-07 00:56:06 -0700

    Add files via upload

---
+++ Q19. Dinner Guests/app.js
@@ -0,0 +1,9 @@
+"use strict";
+// Method 1
+Object.defineProperty(exports, "__esModule", { value: true });
+let guestArray = ["haya", "aresha", "rameema"];
+console.log(guestArray.length, "people are invited to dinner");
+// Method 2
+let guest = ["Ali", "Ahmed", "Rashid"];
+let guestList = guest.length;
+console.log(`We are inviting totat ${guestList} guest.`);
---
+++ Q19. Dinner Guests/app.ts
@@ -0,0 +1,16 @@
+// Method 1
+
+let guestArray:string[] = [ "haya", "aresha", "rameema" ];
+
+console.log(guestArray.length , "people are invited to dinner");
+
+
+
+
+// Method 2
+
+let guest:string[] = ["Ali","Ahmed","Rashid"]
+
+let guestList:number = guest.length;
+
+console.log(`We are inviting totat ${guestList} guest.`)
