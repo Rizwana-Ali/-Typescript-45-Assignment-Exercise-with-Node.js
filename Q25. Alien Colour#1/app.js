55ce1a0067debc06a1af0c9564df1c26f2a64cc7
Rizwana Ali <160421362+Rizwana-Ali@users.noreply.github.com>
2024-06-07 00:05:00 -0700

    Add files via upload

---
+++ Q25. Alien Colour#1/app.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// Create a veriable called alien_color
+let alienColor = "green";
+if (alienColor === "green") {
+    console.log("player just earned  5 points!");
+}
+if (alienColor === "blue") {
+    console.log("Aliencolor is blue.");
+}
---
+++ Q25. Alien Colour#1/app.ts
@@ -0,0 +1,14 @@
+// Create a veriable called alien_color
+let alienColor:string = "green";
+
+
+if (alienColor ==="green"){
+    console.log("player just earned  5 points.");
+}
+
+
+
+if (alienColor === "blue"){
+    console.log("Aliencolor is blue.");
+}
+
