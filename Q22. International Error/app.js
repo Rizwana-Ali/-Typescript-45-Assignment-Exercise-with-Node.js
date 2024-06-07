6ddf45bf86b0218bd9b2513dcfc437d615fc3bdd
Rizwana Ali <160421362+Rizwana-Ali@users.noreply.github.com>
2024-06-07 00:28:34 -0700

    Add files via upload

---
+++ Q22. International Error/app.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const days = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
+console.log(days[7]);
+console.log(days[6]);
+console.log(days[5]);
---
+++ Q22. International Error/app.ts
@@ -0,0 +1,8 @@
+const days:string[] =  ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
+
+// Producing Error! by acceccing invalid index of array
+console.log(days[7]);
+
+// Error Removed
+console.log(days[6]);
+console.log(days[5]);
