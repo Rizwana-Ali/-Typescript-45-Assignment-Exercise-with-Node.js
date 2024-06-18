53143d4e88978c5f14b420841f1e0ca74c22dac6
Rizwana Ali <160421362+Rizwana-Ali@users.noreply.github.com>
2024-06-17 23:48:21 -0700

    Add files via upload

---
+++ Q5. Famous Quote_2/app.js
@@ -0,0 +1,5 @@
+let famous_person = "Nelson Mandela";
+let famous_quote = '"Education is the most powerful weapon which you can use to change the world."';
+let message = `${famous_person} once said,${famous_quote}`;
+console.log(message);
+export {};
---
+++ Q5. Famous Quote_2/app.ts
@@ -0,0 +1,8 @@
+let famous_person:string = "Nelson Mandela"
+let famous_quote:string = '"Education is the most powerful weapon which you can use to change the world."';
+
+
+let message:string = `${famous_person} once said,${famous_quote}`;
+
+
+console.log(message);
