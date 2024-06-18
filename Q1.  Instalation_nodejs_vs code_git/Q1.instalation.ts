2fdeb790937777e4c0571f3cad88d09ec69540ef
Rizwana Ali <160421362+Rizwana-Ali@users.noreply.github.com>
2024-06-18 00:34:06 -0700

    Add files via upload

---
+++ Q1.  Instalation_nodejs_vs code_git/Q1.instalation.ts
@@ -0,0 +1,7 @@
+// 1.Install  [Node.js] (https://nodejs.org/en/), 
+// [Typescript](https://www.typescriptlang.org/download)  and  
+// [VS Code] (https://code.visualstudio.com/) on your computer.
+
+// tsc --Init
+  // npm init -y
+  // npm i @types/node 
