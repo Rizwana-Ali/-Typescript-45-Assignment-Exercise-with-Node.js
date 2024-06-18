8d766703faeaf9eea3090dcb3f4a3971cc204741
Rizwana Ali <160421362+Rizwana-Ali@users.noreply.github.com>
2024-06-17 23:39:21 -0700

    Add files via upload

---
+++ Q6. Stripping Names/app.js
@@ -0,0 +1,9 @@
+let whiteSpace = "\n\t humza baig\t\n";
+console.log(whiteSpace);
+let withoutwhiteSpace = whiteSpace.trim();
+console.log(withoutwhiteSpace);
+let fname = "\n\thumza baig\t\n";
+let fname1 = "humza\nbaig";
+console.log(fname);
+console.log(fname1);
+export {};
---
+++ Q6. Stripping Names/app.ts
@@ -0,0 +1,23 @@
+
+let whiteSpace:string = "\n\t humza baig\t\n";
+console.log(whiteSpace);
+
+let WithoutwhiteSpace = whiteSpace.trim()
+
+console.log(WithoutwhiteSpace)
+
+
+
+
+let fname:string ="\n\thumza baig\t\n";
+
+let fname1:string = "humza\nbaig";
+
+console.log(fname);
+console.log(fname1);
+
+
+
+
+
+
